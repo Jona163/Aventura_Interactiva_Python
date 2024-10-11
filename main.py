87bb90c9360c5b71ff41bbbfe90d4f3e66cc88b7
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-10 18:10:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -51,3 +51,9 @@ def combate(personaje, enemigo):
     """Sistema de combate."""
     vida_personaje = 100
     vida_enemigo = 50
+
+
+    while vida_personaje > 0 and vida_enemigo > 0:
+        imprimir_lento(f"Tu vida: {vida_personaje} | Vida del {enemigo}: {vida_enemigo}\n")
+        imprimir_lento("1. Atacar")
+        imprimir_lento("2. Huir\n")
