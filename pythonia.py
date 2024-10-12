cf599efe96ca132e13736943407e81a330f297ee
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-11 19:22:43 -0600

    Update pythonia.py

--- pythonia.py
+++ pythonia.py
@@ -3,3 +3,7 @@
 # Descripción: Juego simple en Python que simula un encuentro con enemigos
 # y la obtención de tesoros. Incluye efectos visuales con colores.
 # GitHub: https://github.com/Jona163
+
+#Importacion de librerias a ocupar:
+import random
+import time
