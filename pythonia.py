3d503a9d3dcf7cc5a37d2065b588a5b2f1e46a8b
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-11 19:22:05 -0600

    Create pythonia.py

---
+++ pythonia.py
@@ -0,0 +1,5 @@
+# Autor: Jonathan Hernández
+# Fecha: 11 octubre 2024
+# Descripción: Juego simple en Python que simula un encuentro con enemigos
+# y la obtención de tesoros. Incluye efectos visuales con colores.
+# GitHub: https://github.com/Jona163
