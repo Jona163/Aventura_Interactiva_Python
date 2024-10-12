e874263d48b2f3184e24f8b83b556c3662803b66
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-11 19:23:02 -0600

    Update pythonia.py

--- pythonia.py
+++ pythonia.py
@@ -7,3 +7,13 @@
 #Importacion de librerias a ocupar:
 import random
 import time
+
+# Colores para la consola
+class Color:
+    RESET = "\033[0m"
+    ROJO = "\033[31m"
+    VERDE = "\033[32m"
+    AZUL = "\033[34m"
+    AMARILLO = "\033[33m"
+    CIAN = "\033[36m"
+    MAGENTA = "\033[35m"
